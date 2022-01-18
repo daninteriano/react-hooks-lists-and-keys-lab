299ba038dbbf38663e45a4375dea73785c48c6b1
Daniel Interiano <dannyboyint@gmail.com>
2022-01-18 12:27:14 -0500

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
   const linkTree = links.map((link) => {
-    return <a key={link} href={`#{link}`}>{link}</a>;
+    return <a key={link} href={"#"+link}>{link}</a>;
   })
   return <nav>{linkTree}</nav>;
 }
